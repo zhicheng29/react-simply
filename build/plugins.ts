72400694bd57fc1e5a0a0a633d3b58ff103a1d18
王智成 <zhicheng@MacBook-Pro.local>
2024-04-15 17:06:18 +0800

    fix: 修复图片压缩问题

--- build/plugins.ts
+++ build/plugins.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 
 import { visualizer } from "rollup-plugin-visualizer";
 import viteCompress from "vite-plugin-compression";
-import viteImagemin from "vite-plugin-imagemin";
+// import viteImagemin from "vite-plugin-imagemin";
 
 import type { PluginOption } from "vite";
 
@@ -31,45 +31,45 @@ function compressMode(viteEnv: ViteEnvType): PluginOption[] {
   return plugins;
 }
 
+/* 关闭图片压缩，有些图片会失真 */
 // 图片压缩
-function imageMode(): PluginOption[] {
-  return viteImagemin({
-    gifsicle: {
-      optimizationLevel: 7,
-      interlaced: false
-    },
-    // 无损压缩配置
-    optipng: {
-      optimizationLevel: 7
-    },
-    mozjpeg: {
-      quality: 20
-    },
-    // 有损压缩配置
-    pngquant: {
-      quality: [0.8, 0.9],
-      speed: 4
-    },
-    // svg 优化
-    svgo: {
-      plugins: [
-        {
-          name: "removeViewBox"
-        },
-        {
-          name: "removeEmptyAttrs",
-          active: false
-        }
-      ]
-    }
-  });
-}
+// function imageMode(): PluginOption[] {
+//   return viteImagemin({
+//     gifsicle: {
+//       optimizationLevel: 7,
+//       interlaced: false
+//     },
+//     // 无损压缩配置
+//     optipng: {
+//       optimizationLevel: 7
+//     },
+//     mozjpeg: {
+//       quality: 20
+//     },
+//     // 有损压缩配置
+//     pngquant: {
+//       quality: [0.8, 0.9],
+//       speed: 4
+//     },
+//     // svg 优化
+//     svgo: {
+//       plugins: [
+//         {
+//           name: "removeViewBox"
+//         },
+//         {
+//           name: "removeEmptyAttrs",
+//           active: false
+//         }
+//       ]
+//     }
+//   });
+// }
 
 function getPlugins(viteEnv: ViteEnvType): PluginOption[] {
   const { VITE_REPORT } = viteEnv;
   return [
     react(),
-    imageMode(),
     compressMode(viteEnv),
     VITE_REPORT && visualizer({ filename: "report.html", gzipSize: true, brotliSize: true, open: true })
   ];
